6b41cdda376f0dd3558b2d27f96cc47013fdcbc8
Daniel Riissanen <daniel.riissanen@gmail.com>
2019-08-23 15:21:19 +0300

    Add Player movement

---
+++ Assets/Scripts/FollowObject.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class FollowObject : MonoBehaviour {
+    public Transform target;
+    public float smoothTime = 0.2f;
+    [Tooltip("Units per second")]
+    public float maxSpeed = Mathf.Infinity;
+    public Vector3 offset;
+
+    private Vector3 velocity = Vector3.zero;
+
+    void LateUpdate()
+    {
+        Vector3 desiredPosition = offset + target.position;
+        Vector3 smoothedPosition = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, smoothTime, maxSpeed);
+        transform.position = smoothedPosition;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Tooltip("Units per second")]
+    public float maxSpeed = 1;
+
+    void Update()
+    {
+        float inputDeltaX = Input.GetAxis("Horizontal");
+        float translateX = maxSpeed * inputDeltaX * Time.deltaTime;
+        transform.Translate(Vector3.right * translateX);
+    }
+}
