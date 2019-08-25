bd62dff6040a03e9608c98ecf097af354e518bbb
Daniel Riissanen <daniel.riissanen@gmail.com>
2019-08-25 15:05:54 +0300

    Add fade in to scenes

---
+++ Assets/Scripts/Behaviour/FadeIn.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class FadeIn : MonoBehaviour
+{
+    [Tooltip("Duration in seconds")]
+    public float duration;
+    [Tooltip("Ease function")]
+    public Ease ease = Ease.Linear;
+
+    private Image image;
+
+    void Start()
+    {
+        image = GetComponent<Image>();
+        image.color = Color.black;
+        image.DOFade(0, duration).SetEase(ease);
+    }
+}
