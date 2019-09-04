28af74ad740cd64fcead95fad06a1026c1da8a42
Daniel Riissanen <daniel.riissanen@gmail.com>
2019-09-05 02:55:47 +0300

    Fix player reset in room 4

--- Assets/Scripts/Behaviour/Room4Controller.cs
+++ Assets/Scripts/Behaviour/Room4Controller.cs
@@ -6,24 +6,26 @@ public class Room4Controller : RoomController
 {
     #region fields
     [SerializeField]
-    private Transform player;
+    private GameObject player;
     [SerializeField]
     private GameObject roomTransitionObject;
     [SerializeField]
     private Text[] texts;
 
     private RoomTransition roomTransition;
+    private Rigidbody2D playerBody;
     #endregion
 
     void Start()
     {
         Debug.Assert(texts.Length > 0);
         roomTransition = roomTransitionObject.GetComponent<RoomTransition>();
+        playerBody = player.GetComponent<Rigidbody2D>();
     }
 
     protected override void OnUpdate()
     {
-        if (player.position.y < -10)
+        if (player.transform.position.y < -10)
             roomTransition.GotoRoom(0, true);
     }
 
@@ -44,7 +46,21 @@ public class Room4Controller : RoomController
             seq.Append(texts[i].DOFade(0, 1));
         }
         seq.Play();
+
+        // Activate player's rigidbody for this room
+        playerBody.simulated = true;
     }
 
-    protected override void OnDeactivate() {}
+    protected override void OnDeactivate()
+    {
+        // Deactivate and reset player's rigidbody, since it's no longer needed
+        playerBody.simulated = false;
+        playerBody.rotation = 0;
+        playerBody.velocity = Vector2.zero;
+        playerBody.angularVelocity = 0;
+        playerBody.inertia = 0;
+
+        // Also reset the transform rotation
+        player.transform.rotation = Quaternion.identity;
+    }
 }
