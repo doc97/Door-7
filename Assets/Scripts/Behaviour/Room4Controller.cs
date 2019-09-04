839437b6aba4d9cfd6b4e33cd2121d1cf6311f6a
Daniel Riissanen <daniel.riissanen@gmail.com>
2019-09-05 02:32:16 +0300

    Add room 4 texts

--- Assets/Scripts/Behaviour/Room4Controller.cs
+++ Assets/Scripts/Behaviour/Room4Controller.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
 
 public class Room4Controller : RoomController
 {
@@ -7,12 +9,15 @@ public class Room4Controller : RoomController
     private Transform player;
     [SerializeField]
     private GameObject roomTransitionObject;
+    [SerializeField]
+    private Text[] texts;
 
     private RoomTransition roomTransition;
     #endregion
 
     void Start()
     {
+        Debug.Assert(texts.Length > 0);
         roomTransition = roomTransitionObject.GetComponent<RoomTransition>();
     }
 
@@ -22,6 +27,24 @@ public class Room4Controller : RoomController
             roomTransition.GotoRoom(0, true);
     }
 
-    protected override void OnActivate() {}
+    protected override void OnActivate()
+    {
+        texts[0].color = Color.white;
+        for (int i = 1; i < texts.Length; ++i)
+            texts[i].color = new Color(1, 1, 1, 0);
+
+        Sequence seq = DOTween.Sequence();
+        seq.AppendInterval(5);
+        seq.Append(texts[0].DOFade(0, 1));
+        for (int i = 1; i < texts.Length; ++i)
+        {
+            seq.AppendInterval(0.5f);
+            seq.Append(texts[i].DOFade(1, 1));
+            seq.AppendInterval(3.5f);
+            seq.Append(texts[i].DOFade(0, 1));
+        }
+        seq.Play();
+    }
+
     protected override void OnDeactivate() {}
 }
